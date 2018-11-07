73bd922e461b50814dc301a982c49dc37ab678fd
투비소프트 <@DESKTOP-37EPPHU>
2018-11-07 14:23:44 +0900

    fithy

--- src/park/calendar/Calender.java
+++ src/park/calendar/Calender.java
@@ -10,8 +10,8 @@ public class Calender {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printMsg(int month, int day) {
-		System.out.printf("%d월은 %d일까지 있습니다.", month, day);
+	public void printMsg(int month, int day, int repeat) {
+		System.out.printf("%d월은 %d일까지 있습니다.[%d]\n", month, day, repeat+1);
 	}
 
 	public static void main(String[] args) {
@@ -20,11 +20,18 @@ public class Calender {
 		Scanner scanner = new Scanner(System.in);
 		Calender calendar = new Calender();
 
-		System.out.println("달을 입력하세요");
+		System.out.println("반복 횟수를 입력하세요.");
+		
+		int repeat = scanner.nextInt();
+		
+		System.out.println("달을 입력하세요.");
 
 		month1 = scanner.nextInt();
 
-		calendar.printMsg(month1, calendar.maxDays(month1));
+		for (int i = 0; i < repeat; i++) {
+			calendar.printMsg(month1, calendar.maxDays(month1), i
+					);
+		}
 
 		scanner.close();
 
